e55cf4b989d19e420630d190466b6cf9389cd51d
Ming Yoo <mingyouagi@qq.com>
2026-09-05 00:48:21 +0800

    refactor(store): remove legacy delivery sqlite surface

--- packages/store/src/index.ts
+++ packages/store/src/index.ts
@@ -1,9 +1,5 @@
-export * from "./delivery-payload-custody.js";
-export * from "./delivery-repository.js";
-export * from "./delivery-schema.js";
 export {
   createPairedRunnerRepository,
   type PairedRunnerRepository,
 } from "./repository.js";
 export { migratePairedRunnerSchema } from "./schema.js";
-export * from "./slack-installation-registry.js";
